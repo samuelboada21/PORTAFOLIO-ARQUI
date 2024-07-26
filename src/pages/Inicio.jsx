3a406969227aeb0b24514877692b59a6b6b841e2
samuelboada21 <samuelomarboba@ufps.edu.co>
2024-07-26 16:18:35 -0500

    cambio react

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,38 +1,39 @@
-import { Box, Button, Flex, Link, Text } from "@chakra-ui/react";
-import { useRef, useState, useEffect } from "react";
+import Slider from "react-slick";
+import { Flex, Box, Text, Button, Link } from "@chakra-ui/react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useRef } from "react";
 import { FaArrowDown } from "react-icons/fa";
 import Servicios from "../components/servicios";
 import InicioNosotros from "./InicioNosotros";
 import InicioTrabajo from "./InicioTrabajo";
 
-function Inicio() {
-  // const [w] = useMediaQuery("(min-width: 700px)");
-  const [currentImage, setCurrentImage] = useState(0);
-  const [transitionEnabled, setTransitionEnabled] = useState(true);
-  const images = [
-    { src: "./RENDERS/COCINA_INTERIOR/ci1.jpg", text: "BIENVENIDO" },
-    { src: "./RENDERS/COCINA_INTERIOR/ci2.jpg", text: "Visualización" },
-    { src: "./RENDERS/HABITACION/h1.jpg", text: "Inspiración" },
-    { src: "./RENDERS/EDIFICIO/e3.jpg", text: "Transformación" },
-    { src: "./nosotros/Nosotros1.png", text: "Innovación" },
-  ];
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTransitionEnabled(true);
-      setCurrentImage((prevImage) => {
-        if (prevImage >= images.length) {
-          setTimeout(() => {
-            setTransitionEnabled(false);
-            setCurrentImage(0);
-          }, 1000);
-          return prevImage;
-        }
-        return prevImage + 1;
-      });
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [images.length]);
+// Carrusel de imágenes
+const carouselImages = [
+  { src: "./RENDERS/COCINA_INTERIOR/ci1.jpg", text: "BIENVENIDO" },
+  { src: "./RENDERS/COCINA_INTERIOR/ci2.jpg", text: "Visualización" },
+  { src: "./RENDERS/ZONA_TVs/zt2.png", text: "Inspiración" },
+  { src: "./RENDERS/EDIFICIO/e2.jpg", text: "Transformación" },
+  { src: "./nosotros/Nosotros1.png", text: "Innovación" },
+];
 
+const settings = {
+  dots: false, // Desactiva los puntos de navegación
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 1000,
+  autoplaySpeed: 3000,
+  cssEase: "linear",
+  arrows: false, // Desactiva las flechas de navegación
+  draggable: false, // Desactiva la capacidad de arrastrar las imágenes
+  swipe: false, // Desactiva la capacidad de deslizar con el dedo
+  touchMove: false, // Desactiva el movimiento táctil
+  pauseOnHover: false, // Asegura que el autoplay no se detenga en hover
+};
+
+export default function Inicio() {
   const serviciosRef = useRef(null);
 
   const scrollToServicios = () => {
@@ -73,27 +74,9 @@ function Inicio() {
         overflow="hidden"
         position="relative"
       >
-        <Box
-          className="carousel"
-          display="flex"
-          flexDirection="row"
-          transition={transitionEnabled ? "transform 1s ease-in-out" : "none"}
-          transform={`translateX(-${currentImage * 100}%)`}
-          width="100%"
-          height="100%"
-          position="absolute"
-          top="0"
-          left="0"
-          zIndex="0"
-        >
-          {images.map((image, index) => (
-            <Box
-              key={index}
-              flex="none"
-              width="100%"
-              height="100%"
-              position="relative"
-            >
+        <Slider {...settings} style={{ width: "100%" }}>
+          {carouselImages.map((image, index) => (
+            <Box key={index} position="relative" height="100vh" width="100%">
               <Box
                 backgroundImage={`url(${image.src})`}
                 backgroundSize="cover"
@@ -112,7 +95,7 @@ function Inicio() {
                 alt={image.text}
                 width="100%"
                 height="100%"
-                objectFit="contain"
+                objectFit="cover"
                 opacity={0.9}
                 position="absolute"
                 top="0"
@@ -140,73 +123,13 @@ function Inicio() {
               </Flex>
             </Box>
           ))}
-          {/* Duplicating the first image to achieve smooth transition */}
-          <Box flex="none" width="100%" height="100%" position="relative">
-            <Box
-              backgroundImage={`url(${images[0].src})`}
-              backgroundSize="cover"
-              backgroundPosition="center"
-              filter="blur(8px)"
-              width="100%"
-              height="100%"
-              position="absolute"
-              top="0"
-              left="0"
-              zIndex="0"
-            />
-            <Box
-              as="img"
-              src={images[0].src}
-              alt={images[0].text}
-              width="100%"
-              height="100%"
-              objectFit="contain"
-              opacity={0.9}
-              position="absolute"
-              top="0"
-              left="0"
-              zIndex="1"
-            />
-            <Flex
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              position="absolute"
-              top="55%"
-              left="50%"
-              transform="translate(-50%, -50%)"
-              zIndex="2"
-              textAlign="center"
-              color="white"
-              bg="rgba(0,0,0,0.5)"
-              p={5}
-              borderRadius="md"
-            >
-              <Text fontSize={["lg", "3xl", "4xl", "5xl"]} fontWeight="bold">
-                {images[0].text}
-              </Text>
-            </Flex>
-          </Box>
-        </Box>
+        </Slider>
         <Flex
           direction="column"
           alignItems="center"
           justifyContent="center"
           zIndex="1"
         >
-          {/* <Text
-            fontSize={["5xl", "6xl"]}
-            fontWeight="bold"
-            className="slide-in-left"
-            mb={"120px"}
-          >
-            <Text className="changeText" as="span" color="#1E1D1D" mr="5px">
-              Bienvenido
-            </Text>
-          </Text>
-          <Text fontWeight="bold" className="slide-in-right" fontSize="xl">
-            Nos complace tu visita
-          </Text> */}
           <Button
             onClick={scrollToServicios}
             position="absolute"
@@ -220,7 +143,6 @@ function Inicio() {
             className="service-image"
             borderColor={"white"}
             p={4}
-            zIndex="2"
             ref={serviciosRef}
             boxShadow="lg"
           >
@@ -252,5 +174,3 @@ function Inicio() {
     </>
   );
 }
-
-export default Inicio;
