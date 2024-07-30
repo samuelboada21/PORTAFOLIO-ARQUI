71c788ee771587d1e278e76d9226592e41005b2c
samuelboada21 <samuelomarboba@ufps.edu.co>
2024-07-30 10:27:17 -0500

    inicio speed

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -23,7 +23,7 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  speed: 1000,
+  speed: 400,
   autoplaySpeed: 3000,
   cssEase: "linear",
   arrows: false, // Desactiva las flechas de navegación
